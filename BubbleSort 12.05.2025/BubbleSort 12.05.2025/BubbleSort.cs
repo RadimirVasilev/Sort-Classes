6526d3bd435d9dc845bc43d84c4b8b0e54b44241
Radimir Tenev Vasilev <159330968+RadimirVasilev@users.noreply.github.com>
2025-05-13 09:57:16 +0300

    Bubble Sort added

---
+++ BubbleSort 12.05.2025/BubbleSort 12.05.2025/BubbleSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort_12._05._2025
+{
+    internal class BubbleSort
+    {
+
+       public static void Sort<T>(T[] a) where T: IComparable<T>
+        {
+            bool swapped;
+            do 
+            {
+                swapped = false;
+                for(int i=0; i < a.Length - 1; i++)
+                {
+                    if(Less(a[i], a[i+1]))
+                    {
+                        Swap(a, i, i+1);
+                        swapped = true;
+                    }
+                }
+            } 
+            while (swapped);
+        }
+
+
+        public static bool Less<T>(T first, T second) where T : IComparable<T>
+        {
+            return first.CompareTo(second) > 0;
+        }
+
+        public static void Swap<T>(T[] a, int i, int j) where T : IComparable<T>
+        {
+            T temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+
+        }
+
+        
+
+        
+    }
+}
---
+++ BubbleSort 12.05.2025/BubbleSort 12.05.2025/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace BubbleSort_12._05._2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 8, 3, 1, 546, 3, };
+            BubbleSort.Sort(arr);
+            Console.WriteLine(string.Join(" ", arr));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

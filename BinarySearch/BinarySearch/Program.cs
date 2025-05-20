0c35123e91053c00551bedd5662736a7b4b8a95d
Radimir Tenev Vasilev <159330968+RadimirVasilev@users.noreply.github.com>
2025-05-20 11:08:12 +0300

    Add files via upload
    Cheat Code for binary sorting

---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.Design;
+
+namespace BinarySearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 15, 16, 17, 18 };
+
+            int index = BinarySearch(nums, 15, 0, nums.Length - 1);
+
+            Console.WriteLine($"index is {index}, num = {nums[index]}");
+        }
+
+        public static int BinarySearch(int[] arr, int find, int start, int end)
+        {
+            if(end < start)
+            {
+                return -1;
+            }
+            else
+            {
+                int mid = (start + end) / 2;
+                if (arr[mid] > find)
+                {
+                    return BinarySearch(arr, find, start, mid - 1);
+                }
+                else if (arr[mid] < find)
+                {
+                    return BinarySearch(arr, find, mid + 1, end);
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+            
+        }
+    }
+}
